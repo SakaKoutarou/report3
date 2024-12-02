c8b11300447f43a178ff06928bce2a13c3f310aa
Saka Koutarou <e245707@ie.u-ryukyu.ac.jp>
2024-12-03 03:04:07 +0900

    report3_3

--- app/src/main/java/jp/ac/uryukyu/ie/e245707/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245707/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e245707;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -36,7 +36,7 @@ public class Enemy {
     public void attack(Hero hero){
         if (hitPoint > 0){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
         }
     }
@@ -53,5 +53,28 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
-
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+    public void setHitPoint(int hitpoint){
+        this.hitPoint = hitPoint;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public void setAttack(){
+        this.attack = attack;
+    }
+    public boolean getDead(){
+        return this.dead;
+    }
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
 }
